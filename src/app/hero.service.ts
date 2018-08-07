8cb269955c1343c9ef92f1f17e872a01a1a71a91
AndreasMN <andycutter@godmail.dk>
2018-08-07 10:03:05 +0200

    refactor getHero to not use HEROES any more, add error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log('fetched heroes')),
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    debugger;
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`))
+    );
   }
 
   constructor(
@@ -30,5 +35,14 @@ export class HeroService {
   }
 
   private heroesUrl = 'api/heroes';
+
+  private handleError<T> (operation = 'operation', result?: T)
+  {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
   
 }
